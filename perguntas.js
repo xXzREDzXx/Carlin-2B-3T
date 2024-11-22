a608e6d4ebfa9b4bd99067e96589532266f73912
xXzREDzXx <142517088+xXzREDzXx@users.noreply.github.com>
2024-11-22 15:15:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'POKÉMON',
+    'Qual é o maior Pokémon do mundo??',
+    'Eternatus'
+)
+
+criaCartao(
+    'POKÉMON',
+    'Qual é o Pokémon 1000?',
+    'Gholdengo'
+)
+
+criaCartao(
+    'POKÉMON',
+    'Qual é o Pokémon mais raro?',
+    'Ditto'
+)
+
+criaCartao(
+    'POKÉMON',
+    'Qual é o Pokémon 1?',
+    'Bulbassauro'
+)
